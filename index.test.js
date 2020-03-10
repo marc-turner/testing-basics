bc0cc121246c3d215f38d4fe9e53600df1e24553
Marc Turner <marc.t1967@gmail.com>
2020-03-10 13:21:29 -0400

    add final version basics

---
+++ index.test.js
@@ -0,0 +1,40 @@
+const assert = require('assert');
+const { forEach, map } = require('./index');
+
+const test = (desc, fn) => {
+    console.log('---', desc);
+    try {
+        fn();
+    } catch (err) {
+        console.log(err.message);
+    }
+};
+
+test('The forEach function', () => {
+    let sum = 0;
+    forEach([1, 2, 3], value => {
+        sum += value;
+    });
+
+    assert.strictEqual(sum, 6, 'Expected forEach to sum the array');
+});
+
+test('The map function', () => {
+    const result = map([1, 2, 3], value => {
+        return value * 2;
+    });
+
+    assert.deepStrictEqual(result, [2, 4, 6]);
+    // assert.strictEqual(result[0], 2);
+    // if (result[0] !== 2) {
+    //     throw new Error(`Expected to find 2, but found ${result[0]}`);
+    // }
+    // assert.strictEqual(result[1], 4);
+    // if (result[1] !== 4) {
+    //     throw new Error(`Expected to find 4, but found ${result[0]}`);
+    // }
+    // assert.strictEqual(result[2], 6);
+    // if (result[2] !== 6) {
+    //     throw new Error(`Expected to find 6 but found ${result[0]}`);
+    // }
+});
